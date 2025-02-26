b1af7560e5ea1819cbb72365750c5550cd1a0cb9
rodridelossantos <137809941+rodridelossantos@users.noreply.github.com>
2025-02-26 03:44:08 -0300

    add remove task functionality

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ const app = document.getElementById('app')
 const tasksHeader = document.getElementById('tasks_header')
 const addTaskBtn = document.getElementById("add_task_btn")
 const taskInput = document.getElementById("task_input")
-const tasksContainer = document.getElementById("tasks_container")
+const tasksContainer = document.getElementById("tasks_container");
 
 tasks.style = "display: none"
 
@@ -68,8 +68,49 @@ addTaskBtn.addEventListener('click', () => {
 const displayTasks = (list) => {
     let tasksHtml = ``;
     for (let i = 0; i < list.tasks.length; i++) {
-        tasksHtml += `<div class='task' id='${i}'>${list.tasks[i].task}</div>`
+        tasksHtml += `<div style="${list.tasks[i].completed ? "text-decoration: line-through" : ""}" class='task' id='${i}'>${list.tasks[i].task}
+        <svg class="trash_icon" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
+  <path stroke-linecap="round" stroke-linejoin="round" d="m14.74 9-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 0 1-2.244 2.077H8.084a2.25 2.25 0 0 1-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 0 0-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 0 1 3.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 0 0-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 0 0-7.5 0" />
+</svg>
+</div>`
+
     }
     console.log(tasksHtml)
     tasksContainer.innerHTML = tasksHtml;
+    appendCompleteTaskListener(tasksContainer.children)
+    appendRemoveTaskListener(tasksContainer.children)
+
+}
+
+const appendCompleteTaskListener = (tasks) => {
+    let currentList = listArr.find((e) => e.name === tasksHeader.id)
+    for (let i = 0; i < tasks.length; i++) {
+        tasks[i].addEventListener('click', (e) => {
+            console.log(e.target)
+            let taskToComplete = currentList.tasks.find((element) => element.task === e.target.textContent)
+            taskToComplete.completed = !taskToComplete.completed;
+            taskToComplete.completed ? e.target.style = "text-decoration: line-through" : e.target.style = "";
+        }
+
+    )
+    }
+}
+
+const appendRemoveTaskListener = (tasks) => {
+    let currentList = listArr.find((e) => e.name === tasksHeader.id)
+    for (let i = 0; i < tasks.length; i++) {
+        console.log(tasks[i].children[i])
+        tasks[i].children[i].addEventListener('click', (e) => {
+            console.log(e.target)
+            let taskToRemove = currentList.tasks.findIndex((element) => {
+                element.name === e.target.parentElement.textContent;
+            })
+            currentList.tasks.splice(
+                taskToRemove, 1
+            )
+            e.target.parentElement.remove()
+        }
+
+    )
+    }
 }
