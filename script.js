c5a85aac6a0611923b6b55b4f34eef5fe0924ba2
rodridelossantos <137809941+rodridelossantos@users.noreply.github.com>
2025-03-01 02:23:14 -0300

    complete tasks

--- script.js
+++ script.js
@@ -7,6 +7,10 @@ const addTaskBtn = document.getElementById("add_task_btn")
 const taskInput = document.getElementById("task_input")
 const tasksContainer = document.getElementById("tasks_container");
 const backBtn = document.getElementById('back_button')
+const completedTasks = document.getElementById('hide_completed_div')
+
+// Usar clases utilitarias en vez de in-line styles.
+
 
 tasks.style = "display: none"
 
@@ -53,6 +57,8 @@ const attachShowListTasks = () => {
             displayTasks(listArr.find((e) => e.name === listName))
         })
     }
+    
+    console.log(lists.children)
     backBtnAddClickListener();
 }
 
@@ -76,24 +82,41 @@ addTaskBtn.addEventListener('click', () => {
         }
     );
     displayTasks(currentList);
-    
 })
 
 const displayTasks = (list) => {
-    let tasksHtml = ``;
-    for (let i = 0; i < list.tasks.length; i++) {
-        tasksHtml += `<div style="${list.tasks[i].completed ? "text-decoration: line-through" : ""}" class='task' id='${i}'>${list.tasks[i].task}
+    taskHtmlTemplate = (isCompleted, id, task) => {return `<div style="${isCompleted ? "text-decoration: line-through" : ""}" class='task' id='${id}'>${task}
         <svg class="trash_icon" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
   <path style= "pointer-events: none;" stroke-linecap="round" stroke-linejoin="round" d="m14.74 9-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 0 1-2.244 2.077H8.084a2.25 2.25 0 0 1-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 0 0-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 0 1 3.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 0 0-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 0 0-7.5 0" />
 </svg>
-</div>`
-
+</div>`}
+    let pendingTasksHtml = ``;
+    let completedTasksHtml = ``
+    for (let i = 0; i < list.tasks.length; i++) {
+        if (list.tasks[i].completed) {
+            completedTasksHtml += taskHtmlTemplate(list.tasks[i].completed, i, list.tasks[i].task)
+        }
+        else {
+            pendingTasksHtml += taskHtmlTemplate(list.tasks[i].completed, i, list.tasks[i].task)
+        } 
     }
-    console.log(tasksHtml)
-    tasksContainer.innerHTML = tasksHtml;
-    appendCompleteTaskListener(tasksContainer.children)
-    appendRemoveTaskListener(tasksContainer.children)
+    
+    if (
+        !completedTasksHtml
+    ) {
+        completedTasks.setAttribute("hidden", "true")
+    } else {
+        completedTasks.setAttribute("hidden", "false")
+    }
 
+    console.log(pendingTasksHtml)
+
+    tasksContainer.children[0].innerHTML = pendingTasksHtml;
+    tasksContainer.children[2].innerHTML = completedTasksHtml;
+    appendCompleteTaskListener(tasksContainer.children[0].children)
+    appendCompleteTaskListener(tasksContainer.children[2].children)
+    appendRemoveTaskListener(tasksContainer.children[0].children)
+    appendRemoveTaskListener(tasksContainer.children[2].children)
 }
 
 const appendCompleteTaskListener = (tasks) => {
@@ -102,8 +125,10 @@ const appendCompleteTaskListener = (tasks) => {
         tasks[i].addEventListener('click', (e) => {
             console.log(e.target)
             let taskToComplete = currentList.tasks.find((element) => element.task === e.target.innerText)
+            console.log(taskToComplete)
             taskToComplete.completed = !taskToComplete.completed;
             taskToComplete.completed ? e.target.style = "text-decoration: line-through" : e.target.style = "";
+            displayTasks(currentList)
         }
 
     )
