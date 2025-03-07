b662b908a55ab93327b5472488582d92f478c728
rodridelossantos <137809941+rodridelossantos@users.noreply.github.com>
2025-03-07 06:51:35 -0300

    try stuff out

--- script.js
+++ script.js
@@ -9,13 +9,16 @@ const tasksContainer = document.getElementById("tasks_container");
 const backBtn = document.getElementById('back_button')
 const completedTasks = document.getElementById('hide_completed_div')
 
-let emtpyCircle = `<svg class="empty_circle" xmlns="http://www.w3.org/2000/svg" fill="none" stroke="currentColor" stroke-width="1" class="w-6 h-6" viewBox="0 0 24 24"><path stroke-linecap="round" stroke-linejoin="round" d="M21 12a9 9 0 1 1-18 0 9 9 0 0 1 18 0z"/></svg>`
+let emtpyCircle = `
+<svg class="empty_circle" xmlns="http://www.w3.org/2000/svg" fill="none" stroke="currentColor" stroke-width="1" class="w-6 h-6" viewBox="0 0 24 24"><path stroke-linecap="round" stroke-linejoin="round" d="M21 12a9 9 0 1 1-18 0 9 9 0 0 1 18 0z"/></svg> 
+`
 let checkedCircle = `<svg class="checked_circle" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1" stroke="currentColor" class="size-6">
 <path stroke-linecap="round" stroke-linejoin="round" d="M9 12.75 11.25 15 15 9.75M21 12a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z" />
 </svg>`
 let trashIcon = `<svg class="trash_icon" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.2" stroke="currentColor" class="size-6">
 <path style= "pointer-events: none;" stroke-linecap="round" stroke-linejoin="round" d="m14.74 9-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 0 1-2.244 2.077H8.084a2.25 2.25 0 0 1-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 0 0-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 0 1 3.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 0 0-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 0 0-7.5 0" />
 </svg>`
+
 
 // Usar clases utilitarias en vez de in-line styles.
 
@@ -112,12 +115,19 @@ addTaskBtn.addEventListener('click', () => {
     displayTasks(currentList);
 })
 
+const swapIcon = (id, isHovered) => {
+    const iconElement = document.getElementById(`icon-${id}`)
+        iconElement.innerHTML = isHovered ? checkedCircle : emtpyCircle;
+}
+
 const displayTasks = (list) => {
 
     taskHtmlTemplate = (isCompleted, id, task, key) => {return `
         <div style="${isCompleted ? "text-decoration: line-through" : ""}" class='task' key='${key}' id='${id}'>
         <div class="task_name_container">
+        <div id="icon-${id}" class="task_icon">
         ${isCompleted ? checkedCircle : emtpyCircle}
+        </div>
         ${task}
         </div>
         ${trashIcon}
@@ -141,7 +151,7 @@ const displayTasks = (list) => {
         completedTasks.setAttribute("hidden", "false")
     }
 
-    console.log(pendingTasksHtml)
+    // console.log(pendingTasksHtml)
     tasksContainer.children[0].innerHTML = pendingTasksHtml;
     tasksContainer.children[2].innerHTML = completedTasksHtml;
     appendCompleteTaskListener(tasksContainer.children[0].children)
@@ -157,15 +167,17 @@ const uniqueKeyGenerator = () =>{
 const appendCompleteTaskListener = (tasks) => {
     let currentList = listArr.find((e) => e.fullName === tasksHeader.innerText)
     for (let i = 0; i < tasks.length; i++) {
-        tasks[i].addEventListener('click', (e) => {
-            console.log(e.target)
-            let taskToComplete = currentList.tasks.find((element) => element.key === e.target.attributes.key.value)
+        console.log("this is: ", tasks[i].children[0].children[0])
+        tasks[i].children[0].children[0].addEventListener('click', (e) => {
+            e.stopPropagation();
+            console.log("hello", e.target)
+            console.log("element to complete is", e.target.parentElement.parentElement)
+            let taskToComplete = currentList.tasks.find((element) => element.key === e.target.parentElement.parentElement.attributes.key.value)
             console.log(taskToComplete)
             taskToComplete.completed = !taskToComplete.completed;
-            taskToComplete.completed ? e.target.style = "text-decoration: line-through" : e.target.style = "";
+            taskToComplete.completed ? e.target.parentElement.parentElement.style = "text-decoration: line-through" : e.target.parentElement.parentElement.style = "";
             displayTasks(currentList)
         }
-
     )
     }
 }
@@ -177,13 +189,10 @@ const appendRemoveTaskListener = (tasks) => {
             e.stopPropagation();
             console.log(e.target.parentElement.parentElement)
             let taskToRemove = currentList.tasks.findIndex((element) => {
-                element.fullName === e.target.parentElement.innerText;
-            })
-            let taskToRem = currentList.tasks.findIndex((element) => {
                 element.key === e.target.parentElement.attributes.key.value;
             })
             currentList.tasks.splice(
-                taskToRem, 1
+                taskToRemove, 1
             )
             e.target.parentElement.remove()
         }
