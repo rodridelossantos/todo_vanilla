5e3f91c802cf35b848327dde4a10e3bc37bf190c
rodridelossantos <137809941+rodridelossantos@users.noreply.github.com>
2025-03-30 08:14:06 -0300

    complete dark theme toggle

--- script.js
+++ script.js
@@ -26,19 +26,38 @@ const ellipsesSvg = `<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox
   <path stroke-linecap="round" stroke-linejoin="round" d="M12 6.75a.75.75 0 1 1 0-1.5.75.75 0 0 1 0 1.5ZM12 12.75a.75.75 0 1 1 0-1.5.75.75 0 0 1 0 1.5ZM12 18.75a.75.75 0 1 1 0-1.5.75.75 0 0 1 0 1.5Z" />
 </svg>
 `
-const moonSvg = `<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
+const moonSvg = `<svg class="moon_svg" id="moon_svg" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
   <path stroke-linecap="round" stroke-linejoin="round" d="M21.752 15.002A9.72 9.72 0 0 1 18 15.75c-5.385 0-9.75-4.365-9.75-9.75 0-1.33.266-2.597.748-3.752A9.753 9.753 0 0 0 3 11.25C3 16.635 7.365 21 12.75 21a9.753 9.753 0 0 0 9.002-5.998Z" />
 </svg>
 `
-const sunSvg = `<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
+const sunSvg = `<svg class="sun_svg" id="sun_svg" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
   <path stroke-linecap="round" stroke-linejoin="round" d="M12 3v2.25m6.364.386-1.591 1.591M21 12h-2.25m-.386 6.364-1.591-1.591M12 18.75V21m-4.773-4.227-1.591 1.591M5.25 12H3m4.227-4.773L5.636 5.636M15.75 12a3.75 3.75 0 1 1-7.5 0 3.75 3.75 0 0 1 7.5 0Z" />
 </svg>
 `
 
-// Usar clases utilitarias en vez de in-line styles.
 
+if (!localStorage.getItem("dark-theme")){ 
+localStorage.setItem("dark-theme", "true")
+}
 
-// tasks.style = "display: none"
+const setTheme = () => {
+    let isDark = JSON.parse(localStorage.getItem("dark-theme"));
+    if (isDark) {
+        document.body.classList.add("dark_mode")
+        darkModeDiv.innerHTML = moonSvg;
+    }
+    else {
+        document.body.classList.remove('dark_mode')
+        darkModeDiv.innerHTML = sunSvg;
+    }
+}
+setTheme();
+
+darkModeDiv.addEventListener('click', () => {
+    let isDark = JSON.parse(localStorage.getItem("dark-theme"))
+    localStorage.setItem("dark-theme", isDark ? "false" : "true");
+    setTheme()
+})
 
 tasks.classList.add("hidden")
 
